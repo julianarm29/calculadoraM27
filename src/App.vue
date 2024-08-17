80a1c4cbfb1024c776bf1eca3c9bfe8a38c3be9a
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2024-08-17 21:01:33 +0200

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Calculadora Aritmética</h1>
+    <div>
+      <input v-model.number="numero1" type="number" placeholder="Digite o primeiro número" />
+      <input v-model.number="numero2" type="number" placeholder="Digite o segundo número" />
+      
+      <select v-model="operacao">
+        <option value="+">Adicionar</option>
+        <option value="-">Subtrair</option>
+        <option value="*">Multiplicar</option>
+        <option value="/">Dividir</option>
+      </select>
+
+      <p v-if="resultado !== null">Resultado: {{ resultado }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: '+',
+      resultado: null
+    };
+  },
+  watch: {
+    numero1: 'calcular',
+    numero2: 'calcular',
+    operacao: 'calcular'
+  },
+  methods: {
+    calcular() {
+      const num1 = parseFloat(this.numero1);
+      const num2 = parseFloat(this.numero2);
+      switch (this.operacao) {
+        case '+':
+          this.resultado = num1 + num2;
+          break;
+        case '-':
+          this.resultado = num1 - num2;
+          break;
+        case '*':
+          this.resultado = num1 * num2;
+          break;
+        case '/':
+          if (num2 === 0) {
+            this.resultado = 'Erro: Divisão por zero';
+          } else {
+            this.resultado = num1 / num2;
+          }
+          break;
+        default:
+          this.resultado = null;
+      }
+    }
+  },
+  mounted() {
+    this.calcular(); // Calcular inicialmente com os valores padrão
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+input, select {
+  margin: 5px;
+  padding: 10px;
+  font-size: 16px;
 }
 </style>
